a95aa4615ba9edd64527aa0e184c13af9dc8df59
Matt Risley <35645770+MRRisley@users.noreply.github.com>
2018-11-02 16:28:45 -0400

    Add files via upload

---
+++ In-Class-Assignment-2.sql
@@ -0,0 +1,73 @@
+/*** In-Class Assignment 2 ***/
+
+
+/******
+All questions except #1.b MUST be answered with a query
+******/
+
+
+
+/* #1.a: Write a query to select PhoneNumber and Modified Date from
+Person.PersonPhone table in the AdventureWorks Database */
+
+
+
+
+/* #1.b: Write the database name, schema name, and table name in 
+the comments below */
+
+--you can use two dashes (single line)
+--or
+/* slashes and asterisks to comment 
+(can extend to multiple lines)*/
+
+
+
+
+/* #2.a: Select all records from [AdventureWorks].[HumanResources].[Employee] */
+
+
+
+/* #2.b: Write a query to return the number of records in the table? */
+
+
+
+/* #3: Write a query to return the sum of 15.2 and 3.4 in the numerator and 12.6 in the denominator. */
+
+
+
+/* #4: Write a query to return the top 10 records in Person.Address that have a StateProvinceID of 74 */
+
+
+
+/* #5: Sort Production.Document in alphabetical order for the field Title */
+
+
+
+/* #6: Write a query to show the largest quantity in [AdventureWorks].[Production].[ProductInventory]? 
+(Hint: Use keyword desc such that the first record has the largest quantity. You can then use
+select TOP 1 to return only this record) */
+
+
+
+
+/* #7: Return duplicate records (if they exist) for Purchasing.PurchaseOrderDetail for field
+PurchaseOrderID. */
+
+
+
+
+/* #8 Return the CurrencyName for CurrencyCode 'NGN' from table [AdventureWorks].[Sales].[Currency] */
+
+
+
+
+/* #9 Count the number of work orders (WorkOrderID) per product (ProductID) in the table
+[AdventureWorks].[Production].[WorkOrder] and sort the ProductIDs in ascending order. */
+
+
+
+
+/* #10 How many products in [AdventureWorks].[Production].[ProductListPriceHistory] have a list price greater 
+than $1,000? */
+
