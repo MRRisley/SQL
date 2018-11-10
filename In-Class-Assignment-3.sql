adf5b82166c9c91cf48e492667877de8cbc8f5a4
Matt Risley <35645770+MRRisley@users.noreply.github.com>
2018-11-10 10:52:10 -0500

    Add files via upload

---
+++ In-Class-Assignment-3.sql
@@ -0,0 +1,56 @@
+/*** In-Class Assignment 3 ***/
+
+
+
+/* Answer with a query:
+#1: Return the first five fields in [AdventureWorks].[Production].[ProductInventory] 
+for quantities at the record level less than 25. Add a decimal place to the quantity field in the 
+results, and make sure the column name is still 'quantity'. */
+
+
+
+/*Answer with a query and a comment: 
+#2: Return the minimum and maximum Bin in [AdventureWorks].[Production].[ProductInventory]. If
+management was certain the bins would never fall below the minimum or exceed the maximum in the
+future, what data type would require the least amount of space and still preserve the data? */
+
+
+
+/* Answer with three queries:
+#3: Return the first five fields in [AdventureWorks].[Production].[ProductInventory] 
+for inventory that was updated in the year 2014. We will show three ways to accomplish this:
+a. through the YEAR() function
+b. through the AND operator
+c. through the BETWEEN operator */
+
+
+
+/* Answer with a query:
+#4: Return the first six fields in [AdventureWorks].[Production].[ProductInventory] 
+for productIDs 412, 422, and 776. Add a seventh field, rowguid2, based on the field rowguid. 
+From rowguid, rowguid2 should display the first 8 characters,
+followed by a space character, followed by the final 12 characters. This will require the LEFT()
+and RIGHT() functions as well as concatenation. */
+
+
+
+/* Answer with a query:
+#5: What is the maximum character length of the rowguid field in [AdventureWorks].[Production].[ProductInventory]? What is an appropriate
+character data type for this field?*/
+
+
+
+
+/* Answer with a query:
+#6: Return all records from [AdventureWorks].[Production].[ProductInventory] where the modified date has a month of October for the years 
+2012-2015. */
+
+
+
+/* Answer with four querys:
+#7: How many middle names in [AdventureWorks].[Person].[Person] only have the first initial? How many have the full middle name? What 
+other values must be considered if we want to account for all records? Verify the counts from these three queries sum to the total
+number of records in the table.*/
+
+
+
