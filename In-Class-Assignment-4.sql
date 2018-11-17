07be602034a53975dad57ddbbb46a8b5afed960d
Matt Risley <35645770+MRRisley@users.noreply.github.com>
2018-11-17 10:16:23 -0500

    Add files via upload

---
+++ In-Class-Assignment-4.sql
@@ -0,0 +1,18 @@
+/*** In-Class Assignment 4 ***/
+
+/*
+In [HumanResources] DB
+Join relevant fields in Department, Employee, and Shift
+We also want to understand when matches are not found with the joining process.
+*/
+
+/*
+In [Person] DB
+Join relevant fields in Department, Employee, and Shift
+*/
+
+/*
+In [Production] DB
+Join relevant fields in Department, Employee, and Shift
+*/
+
